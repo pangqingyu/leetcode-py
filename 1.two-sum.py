f2a93580b5dbd6ec4002478617beb5bd7c5efd27
pangqingyu <qypang@xreal.com>
2025-08-13 11:50:21 +0800

    1.two-sum

---
+++ 1.two-sum.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        num_dict = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in num_dict:
+                return [num_dict[diff], i]
+            num_dict[num] = i
